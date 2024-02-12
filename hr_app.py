7f6ebb992de0ff0326012a50fa64ee93bcd57e6d
pushpendra tawar <98085926+pushpen93@users.noreply.github.com>
2024-02-12 22:34:58 +0530

    Update hr_app.py

--- hr_app.py
+++ hr_app.py
@@ -4,18 +4,6 @@ uploaded_file = st.file_uploader("Upload your resume", type=["pdf", "png", "jpg"
 
 
 
-st.set_page_config(layout="wide")
-base="dark"
-primaryColor="#BF2A7C" #PINK
-backgroundColor="#FFFFFF" #MAIN WINDOW BACKGROUND COLOR (white)
-secondaryBackgroundColor="#EBF3FC" #SIDEBAR COLOR (light blue)
-textColor="#31333F"
-secondaryColor="#F0F2F6" #dark_blue
-tertiaryColor ="#0810A6"
-light_pink = "#CDC9FA"
-plot_blue_colour="#0810A6" #vibrant blue for plots
-
-
 import fitz  # PyMuPDF
 
 def extract_text_from_pdf(uploaded_file):
