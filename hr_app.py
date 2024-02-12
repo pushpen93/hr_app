6f1eca18905a51b2806567e7f34ddb6ae87a9d38
pushpendra tawar <98085926+pushpen93@users.noreply.github.com>
2024-02-12 21:34:26 +0530

    Update hr_app.py

--- hr_app.py
+++ hr_app.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
 uploaded_file = st.file_uploader("Upload your resume", type=["pdf", "png", "jpg", "jpeg"])
+
+
+import fitz  # PyMuPDF
+
+def extract_text_from_pdf(uploaded_file):
+    text = ""
+    with fitz.open(stream=uploaded_file.read(), filetype="pdf") as doc:
+        for page in doc:
+            text += page.get_text()
+    return text
+
+
+import pytesseract
+from PIL import Image
+import io
+
+def extract_text_from_image(uploaded_file):
+    image = Image.open(io.BytesIO(uploaded_file.read()))
+    text = pytesseract.image_to_string(image)
+    return text
